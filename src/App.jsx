10603e859653f7e0ac51f9367b0fa92a099ccb11
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-07-25 19:17:29 +0300

    chore: initial app setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>React app dockerized</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
